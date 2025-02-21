ecc6a9dd009cfa1e5737ed6dfc619f8899bda0ad
Michal Smyk <michal.smyk38@gmail.com>
2025-02-21 11:57:39 +0000

    finish day 01 exercise level 2

---
+++ Exercises/day_01.py
@@ -0,0 +1,20 @@
+#Exercise level 2 
+
+print(5 + 5) #addition 
+print(4 - 8) #subtraction
+print(2 * 3) # multiplication
+print(5 / 2) # division
+print(3 ** 2) # exponentiation
+print(5 // 2) #flor division
+print(3 % 2) #modulus
+
+#check data types 
+print(type(10)) #int
+print(type(3.14)) #float
+print(type(1 + 3j)) #complex
+print(type('Hello, World!')) #str
+print(type([1, 2, 3])) #list
+print(type({'name': 'John'})) #dictionary
+print(type({9.8, 3.14, 2.7})) #set
+print(type((9.8, 3.14, 2.7))) #tuple
+
