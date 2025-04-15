1b8f567920dad11c257527b27fec0151056d8f2c
MichalSmyk <michal.smyk38@gmail.com>
2025-04-15 15:05:24 +0100

    ex 5 and 6

--- Exercises/day_05.py
+++ Exercises/day_05.py
@@ -8,3 +8,7 @@ print(len(more_than_five))
 print(more_than_five[0]) # first
 print(more_than_five[4:5]) #middle-ish
 print(more_than_five[-1]) # last
+#5
+mixed_data_types = ['Michal', 29, 180, 'married', 'Brighton']
+#6
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
