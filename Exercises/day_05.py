ea43d1df442815ccdc4f0d69f3e83cfc8db30a60
MichalSmyk <michal.smyk38@gmail.com>
2025-04-15 15:02:33 +0100

    ex4

--- Exercises/day_05.py
+++ Exercises/day_05.py
@@ -4,3 +4,7 @@ empty_list = []
 more_than_five = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
 #3
 print(len(more_than_five))
+#4
+print(more_than_five[0]) # first
+print(more_than_five[4:5]) #middle-ish
+print(more_than_five[-1]) # last
